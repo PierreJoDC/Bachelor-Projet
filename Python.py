8dffe97a5ff6927753b07f262e68e3af6c36296e
PierreJoDC <125256462+PierreJoDC@users.noreply.github.com>
2023-02-14 11:26:05 +0100

    Add files via upload

---
+++ Python.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/test/testo', methods=['GET'])
+def hello_world():
+    return 'Noyeu Janiversaire Leny!'
+
+if __name__ == '__main__':
+    app.run(debug=True,port=8080)
